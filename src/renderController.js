ae3f437d976fe25f15b49285fcc3800b54a3695b
Tobe Okafor <teokafor@gmail.com>
2025-01-28 16:16:30 -0600

    Populate cell with today's data

--- src/renderController.js
+++ src/renderController.js
@@ -2,8 +2,34 @@
 // Refactor all necessary doc grabs to initialize function?
 const todayContainer = document.querySelector('.today');
 
-async function drawAll(json) {
-  todayContainer.innerHTML = json['days'][0]['temp'];
+function drawAll(json) {
+  drawTodayCell(json);
 }
+
+
+
+const mainContainer = document.querySelector('.main-container');
+
+function drawTodayCell(json) {
+    const todayElement = document.createElement('div');
+    todayElement.classList.add('today');
+
+    todayElement.innerHTML = `<div class="today-left">
+                    <div class="today-left-left">
+                        <div class="today-icon">${json['days'][0]['icon']}</div>
+                        <div class="today-date">${json['days'][0]['datetimeEpoch']}</div>
+                    </div>
+                    <div class="today-left-right">
+                        <div class="today-temp">${json['days'][0]['temp']}</div>
+                        <div class="today-lo-hi">${json['days'][0]['tempmin']}/${json['days'][0]['tempmax']}</div>
+                        <div class="today-time">${json['days'][0]['datetimeEpoch']}</div>
+                    </div>
+                </div>
+                <div class="today-right"></div>`;
+
+    mainContainer.appendChild(todayElement);
+}
+
+
 
 export { drawAll };
